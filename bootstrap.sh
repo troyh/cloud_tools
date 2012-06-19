6e5c611eef45e5eab3c4c5e5508548db84007ede
Troy Hakala <troy.hakala@gmail.com>
2012-06-19 16:56:02 -0700

    a few more things to do

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,9 @@
 # 
 # Setup script for Rackspace Cloud dev server
 #
+# Usage: bootstrap.sh <GIT_REPO>
+
+GIT_REPO=$1
 
 #apt-get --assume-yes update
 
@@ -24,3 +27,14 @@ apt-get --assume-yes install \
 curl -O http://download.playframework.org/releases/play-2.0.zip
 unzip play-2.0.zip
 export PATH=$PATH:~/play-2.0/
+
+# git clone the source code
+git clone $GIT_REPO
+
+# TODO: Put data in Solr database
+
+# Start Solr
+service jetty start
+
+# Start Play
+play
